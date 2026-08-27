8f29d36be6d962187284f45b7fab6a6951465dd4
blue7012 <pdai.congviec@gmail.com>
2026-08-27 14:28:28 -0400

    task 2 completed

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -117,6 +117,6 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
