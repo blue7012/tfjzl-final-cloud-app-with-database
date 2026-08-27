b54d820d71ce190867a63f4da90a63d894f19c46
blue7012 <pdai.congviec@gmail.com>
2026-08-27 14:16:35 -0400

    task 1 complete

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -51,7 +51,6 @@ class Learner(models.Model):
         return self.user.username + "," + \
                self.occupation
 
-
 # Course model
 class Course(models.Model):
     name = models.CharField(null=False, max_length=30, default='online course')
@@ -75,6 +74,26 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
 
+# Question model
+class Question(models.Model): 
+    course = models.ForeignKey(Course, on_delete=models.CASCADE) 
+    content = models.CharField(max_length=200) 
+    grade = models.IntegerField(default=50)
+    def str(self): 
+        return "Question: " + self.content
+	# method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+class Choice(models.Model): 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    content = models.CharField(max_length=200) 
+    is_correct = models.BooleanField(default=False)
 
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
