29ba3dd61dbf347ab9d5ba4974e9437bbb244ea0
blue7012 <pdai.congviec@gmail.com>
2026-08-27 15:05:52 -0400

    done, fix bug

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -170,5 +170,10 @@ def show_exam_result(request, course_id, submission_id):
     context['course'] = course
     context['grade'] = total_score
     context['choices'] = choices
-
+    
+    return render(
+        request,
+        "onlinecourse/exam_result_bootstrap.html",
+        context
+    )
 
